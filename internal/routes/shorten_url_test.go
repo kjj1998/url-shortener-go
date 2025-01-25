aa9be44b847fca8b9aa91a4afd679404e93a9753
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-01-25 14:46:38 +0800

    completed tests for routes

--- internal/routes/shorten_url_test.go
+++ internal/routes/shorten_url_test.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/gin-gonic/gin"
 	"github.com/kjj1998/url-shortener-go/constants"
 	"github.com/kjj1998/url-shortener-go/internal/models"
@@ -31,11 +34,6 @@ func (m *MockTableClient) Query(ctx context.Context, params *dynamodb.QueryInput
 	args := m.Called(ctx, params)
 	return args.Get(0).(*dynamodb.QueryOutput), args.Error(1)
 }
-
-// func (m *MockTableClient) AddUrl(ctx context.Context, url models.Url) error {
-// 	args := m.Called(ctx, url)
-// 	return args.Error(0)
-// }
 
 func TestGenerateShortenedUrl(t *testing.T) {
 	mockRepo := new(MockTableClient)
@@ -60,6 +58,20 @@ func TestGenerateShortenedUrl(t *testing.T) {
 			mockRepoError:  nil,
 			expectedStatus: http.StatusCreated,
 			expectedBody:   `{"id":2387497,"shortUrl":"NWER425d","longUrl":"http://example.com"}`,
+		},
+		{
+			name:           "Failed JSON binding",
+			payload:        "124",
+			mockRepoError:  nil,
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   `{"code":400, "message":"json: cannot unmarshal number into Go value of type models.LongUrl"}`,
+		},
+		{
+			name:           "Validation error",
+			payload:        map[string]string{"shortUrl": "fdsfsdfdsf"},
+			mockRepoError:  nil,
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   `{"code":400, "message":"invalid parameter names in json body"}`,
 		},
 	}
 
@@ -90,5 +102,107 @@ func TestGenerateShortenedUrl(t *testing.T) {
 
 		assert.Equal(t, tt.expectedStatus, rec.Code)
 		assert.JSONEq(t, tt.expectedBody, rec.Body.String())
+	}
+}
+
+func TestRedirectShortenedUrl(t *testing.T) {
+	tests := []struct {
+		name             string
+		param            string
+		mockRepoError    error
+		expectedStatus   int
+		expectedLocation string
+		mockRepoResult   string
+		mockRepoId       uint64
+	}{
+		{
+			name:             "Valid request",
+			param:            "NWER425d",
+			mockRepoError:    nil,
+			expectedStatus:   http.StatusTemporaryRedirect,
+			expectedLocation: "http://example.com",
+			mockRepoResult:   "http://example.com",
+		},
+		{
+			name:             "No parameter",
+			param:            "",
+			mockRepoError:    nil,
+			expectedStatus:   http.StatusBadRequest,
+			expectedLocation: "",
+			mockRepoResult:   "nil",
+		},
+		{
+			name:             "Internal error",
+			param:            "KWBG425d",
+			mockRepoError:    errors.New("simulated DynamoDB error"),
+			expectedStatus:   http.StatusInternalServerError,
+			expectedLocation: "",
+			mockRepoResult:   "nil",
+		},
+		{
+			name:             "URL not found",
+			param:            "KXBG425d",
+			mockRepoError:    nil,
+			expectedStatus:   http.StatusNotFound,
+			expectedLocation: "",
+			mockRepoResult:   "nil",
+		},
+	}
+
+	for _, tt := range tests {
+		mockRepo := new(MockTableClient)
+		repository.Client = repository.TableClient{
+			DynamoDbClient: mockRepo,
+			TableName:      constants.TableName,
+		}
+
+		if tt.mockRepoError != nil {
+			fmt.Println("test else")
+			mockRepo.On("Query", mock.Anything, mock.Anything).Return(&dynamodb.QueryOutput{}, tt.mockRepoError).Once()
+			mockRepo.On("RetrieveUrl", mock.Anything, tt.param).Return("", tt.mockRepoError).Once()
+		} else {
+			if tt.name == "Valid request" {
+				mockDynamoResponse := &dynamodb.QueryOutput{
+					Items: []map[string]types.AttributeValue{
+						{
+							"ShortUrl": &types.AttributeValueMemberS{Value: tt.param},
+							"LongUrl":  &types.AttributeValueMemberS{Value: tt.mockRepoResult},
+						},
+					},
+				}
+
+				mockRepo.On("Query", mock.Anything, mock.Anything).Return(mockDynamoResponse, tt.mockRepoError).Once()
+			} else if tt.name == "URL not found" {
+				mockDynamoResponse := &dynamodb.QueryOutput{
+					Items: []map[string]types.AttributeValue{
+						{
+							"ShortUrl": &types.AttributeValueMemberS{Value: tt.param},
+							"LongUrl":  &types.AttributeValueMemberS{Value: ""},
+						},
+					},
+				}
+
+				mockRepo.On("Query", mock.Anything, mock.Anything).Return(mockDynamoResponse, tt.mockRepoError).Once()
+			} else {
+				mockRepo.On("Query", mock.Anything, mock.Anything).Return(&dynamodb.QueryOutput{}, tt.mockRepoError).Once()
+			}
+
+			mockRepo.On("RetrieveUrl", mock.Anything, tt.param).Return(tt.mockRepoResult, tt.mockRepoError).Once()
+		}
+
+		rec := httptest.NewRecorder()
+		ctx, _ := gin.CreateTestContext(rec)
+
+		req, _ := http.NewRequest(http.MethodGet, "/api/v1/NWER425d", nil)
+		ctx.Request = req
+
+		ctx.Params = gin.Params{
+			{Key: "shortUrl", Value: tt.param},
+		}
+
+		RedirectShortenedUrl(ctx)
+
+		assert.Equal(t, tt.expectedStatus, rec.Code)
+		assert.Equal(t, tt.expectedLocation, rec.Header().Get("Location"))
 	}
 }
